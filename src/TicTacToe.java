6bd4cf329112f8deefc159771ca3616e7006308c
PrincessMav <princess-iza@hotmail.co.uk>
2021-10-28 20:43:46 +0100

    game complete. added cpu player. added error messages for inhabited boxes.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -130,7 +130,7 @@ import java.util.*;
         List topRow = Arrays.asList(1,2,3);
         List midRow = Arrays.asList(4,5,6);
         List botRow = Arrays.asList(7,8,9);
-        List leftCol = Arrays.asList(1,5,7);
+        List leftCol = Arrays.asList(1,4,7);
         List midCol = Arrays.asList(2,5,8);
         List rightCol = Arrays.asList(3,6,9);
         List cross1 = Arrays.asList(1,5,9);
